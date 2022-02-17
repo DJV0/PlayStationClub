7f509ffaff50c2a078e92de0815139f42b3d6789
Oleh Terentiev <dejavucool97gmail.com>
2022-02-17 12:02:59 +0200

    Configurate image, category and game entities

---
+++ PlayStationClub.Data/Configuration/GameConfigurator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlayStationClub.Data.Entity;
+
+namespace PlayStationClub.Data.Configuration
+{
+    public class GameConfigurator : IEntityTypeConfiguration<Game>
+    {
+        public void Configure(EntityTypeBuilder<Game> builder)
+        {
+            builder.Property(g => g.Name).IsRequired().HasMaxLength(100);
+            builder.Property(g => g.Description).IsRequired();
+            builder.Property(g => g.PlayersNumber).IsRequired();
+
+            builder.HasOne(g => g.Image)
+                .WithOne(i => i.Game)
+                .HasForeignKey<Image>(i=>i.GameId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasMany(g => g.Categories)
+                .WithMany(c => c.Games)
+                .UsingEntity(e => e.HasData(
+                    new { CategoriesId = 1, GamesId = 1 }
+                    ));
+
+            builder.HasData(
+                new Game
+                {
+                    Id = 1,
+                    Name = "mortal kombat 11",
+                    Description = "Новая часть культового файтинга Мортал Комбат, с привычной механикой но множеством нововведений. По мимо новых механик вас ждет обновленная графика и новые персонажи. В остальном это старый добрый МК приходи делать фаталити друзьям.",
+                    PlayersNumber = 2
+                }
+                );
+        }
+    }
+}
---
+++ PlayStationClub.Data/Configuration/ImageConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlayStationClub.Data.Entity;
+
+namespace PlayStationClub.Data.Configuration
+{
+    public class ImageConfigurator : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.Property(i => i.FileName).IsRequired();
+            builder.HasIndex(i => i.FileName).IsUnique();
+
+
+            builder.HasData(
+                new Image { Id = 1, FileName = "game-mortal-kombat", GameId = 1 }
+                //new Image { Id = 2, FileName = "photo-fight" },
+                //new Image { Id = 3, FileName = "photo-race" },
+                //new Image { Id = 4, FileName = "photo-sports" },
+                //new Image { Id = 5, FileName = "photo-team" }
+                );
+        }
+    }
+}
