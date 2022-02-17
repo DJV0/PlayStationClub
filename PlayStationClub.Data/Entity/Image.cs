4cc049b53e04156e0c76cb338cc373782a091f7f
Oleh Terentiev <dejavucool97gmail.com>
2022-02-17 12:02:27 +0200

    Create image, category and game entities

---
+++ PlayStationClub.Data/Entity/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayStationClub.Data.Entity
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
