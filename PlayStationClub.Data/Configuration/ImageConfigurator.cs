032e2a90ac32a1d4b3e9c509509a7946a319143f
Oleh Terentiev <dejavucool97gmail.com>
2022-02-18 16:02:38 +0200

    Seed room data

--- PlayStationClub.Data/Configuration/ImageConfigurator.cs
+++ PlayStationClub.Data/Configuration/ImageConfigurator.cs
@@ -15,9 +15,9 @@ namespace PlayStationClub.Data.Configuration
             builder.HasData(
                 new Image { Id = 1, FileName = "game-mortal-kombat"},
                 new Image { Id = 2, FileName = "photo-fight", RoomId=1 },
-                new Image { Id = 3, FileName = "photo-race", RoomId = 1 },
-                new Image { Id = 4, FileName = "photo-sports", RoomId = 1 },
-                new Image { Id = 5, FileName = "photo-team", RoomId = 1 }
+                new Image { Id = 4, FileName = "photo-sports", RoomId = 2 },
+                new Image { Id = 3, FileName = "photo-race", RoomId = 3 },
+                new Image { Id = 5, FileName = "photo-team", RoomId = 4 }
                 );
         }
     }
