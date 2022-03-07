0d406e74f151b3c95b2449a45799c5876e11b970
Oleh Terentiev <dejavucool97gmail.com>
2022-03-07 18:05:54 +0200

    Configure review entity

---
+++ PlayStationClub.Data/Configuration/ReviewConfigurator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PlayStationClub.Data.Entity;
+
+namespace PlayStationClub.Data.Configuration
+{
+    public class ReviewConfigurator : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.Property(r => r.Text).IsRequired().HasMaxLength(200);
+            builder.Property(r => r.Rating).IsRequired();
+
+            builder.HasData(
+                new Review
+                {
+                    Id = 1,
+                    Text = "text1",
+                    Rating = 5
+                },
+                new Review
+                {
+                    Id = 2,
+                    Text = "text2",
+                    Rating = 2
+                });
+        }
+    }
+}
