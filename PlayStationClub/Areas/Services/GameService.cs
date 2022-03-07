4cd763125f0eeff4ad0351b6ce8e30bcd8809f0f
Oleh Terentiev <dejavucool97gmail.com>
2022-03-07 19:46:16 +0200

    Implement service

---
+++ PlayStationClub/Areas/Services/GameService.cs
@@ -0,0 +1,18 @@
+﻿using PlayStationClub.Areas.Services.Interfaces;
+using PlayStationClub.Data;
+using PlayStationClub.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayStationClub.Areas.Services
+{
+    public class GameService : GenericService<Game>, IGameService
+    {
+        public GameService(PlayStationClubDbContext context) : base(context)
+        {
+
+        }
+    }
+}
