57ae7c6ed7b36a85bf513d101c139a174ae8cce8
Oleh Terentiev <dejavucool97gmail.com>
2022-03-07 18:06:46 +0200

    Configure service entity

--- PlayStationClub.Data/Configuration/ServiceConfigurator.cs
+++ PlayStationClub.Data/Configuration/ServiceConfigurator.cs
@@ -13,7 +13,32 @@ namespace PlayStationClub.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Service> builder)
         {
+            builder.Property(s => s.Name).IsRequired().HasMaxLength(50);
+            builder.Property(s => s.Description).IsRequired();
+            builder.Property(s => s.Price).IsRequired();
 
+            builder.HasData(
+                new Service
+                {
+                    Id = 1,
+                    Name = "s1",
+                    Description = "d1",
+                    Price = 10
+                },
+                new Service
+                {
+                    Id = 2,
+                    Name = "s2",
+                    Description = "d2",
+                    Price = 20
+                },
+                new Service
+                {
+                    Id = 3,
+                    Name = "s3",
+                    Description = "d3",
+                    Price = 30
+                });
         }
     }
 }
