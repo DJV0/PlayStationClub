709456456ebe168b4fd457121ee10d3b58150f4f
Oleh Terentiev <dejavucool97gmail.com>
2022-03-07 18:06:03 +0200

    Create session entity

--- PlayStationClub.Data/Entity/Session.cs
+++ PlayStationClub.Data/Entity/Session.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlayStationClub.Data.Entity
 {
@@ -15,6 +12,12 @@ namespace PlayStationClub.Data.Entity
 
         public int RoomId { get; set; }
         public Room Room { get; set; }
+
+        public string PlayStationClubUserId { get; set; }
+        public PlayStationClubUser PlayStationClubUser { get; set; }
+
+        public int? ReviewId { get; set; }
+        public Review Review { get; set; }
 
         public ICollection<Service> Services { get; set; }
     }
