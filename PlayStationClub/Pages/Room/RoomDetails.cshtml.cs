ceddcb6b157efc2cc4ddd3b57e491ee29a810715
Oleh Terentiev <dejavucool97gmail.com>
2022-03-18 19:03:45 +0200

    Implement game details page

---
+++ PlayStationClub/Pages/Room/RoomDetails.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlayStationClub.Areas.Services.Interfaces;
+using PlayStationClub.Infrastructure.ViewModels;
+
+namespace PlayStationClub.Pages.Room
+{
+    public class RoomDetailsModel : PageModel
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+        public RoomViewModel Room { get; set; }
+
+        public RoomDetailsModel(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+        public async Task OnGet(int id)
+        {
+            Room = _mapper.Map<RoomViewModel>(await _roomService.GetByIdAsync(id));
+        }
+    }
+}
